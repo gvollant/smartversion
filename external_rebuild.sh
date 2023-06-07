59cf9327e7770294a1e78842cbaad1c52aa2e153
Gilles Vollant <info@winimage.com>
2023-06-07 15:22:08 +0200

    update dependency

--- external_rebuild.sh
+++ external_rebuild.sh
@@ -16,8 +16,8 @@ do_download https://github.com/gvollant/lzham_codec/archive/refs/tags/v1_0_stabl
 do_download https://github.com/gvollant/fastlzlib/archive/refs/tags/fastlzlib_with_lzfse.zip fastlzlib.zip
 do_download https://github.com/gvollant/fastlzlib/archive/refs/tags/fastlzlib_with_lzfse.tar.gz fastlzlib.tar.gz
 
-do_download https://github.com/lz4/lz4/archive/refs/tags/v1.9.3.zip lz4.zip
-do_download https://github.com/lz4/lz4/archive/refs/tags/v1.9.3.tar.gz lz4.tar.gz
+do_download https://github.com/lz4/lz4/archive/refs/tags/v1.9.4.zip lz4.zip
+do_download https://github.com/lz4/lz4/archive/refs/tags/v1.9.4.tar.gz lz4.tar.gz
 
 do_download https://github.com/ariya/FastLZ/archive/refs/tags/0.5.0.zip fastlz.zip
 do_download https://github.com/ariya/FastLZ/archive/refs/tags/0.5.0.tar.gz fastlz.tar.gz
@@ -26,9 +26,9 @@ do_download https://sourceware.org/pub/bzip2/bzip2-1.0.5.tar.gz oldbzip2.tar.gz
 do_download https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz bzip2.tar.gz
 
 
-do_download https://www.7-zip.org/a/lzma2107.7z lzma.7z
+do_download https://www.7-zip.org/a/lzma2201.7z lzma.7z
 
-do_download https://tukaani.org/xz/xz-5.4.0.tar.gz xz.tar.gz
+do_download https://tukaani.org/xz/xz-5.4.3.tar.gz xz.tar.gz
 
 do_download https://tukaani.org/xz/xz-embedded-20210201.tar.gz xz-embedded.tar.gz
 
@@ -36,8 +36,8 @@ do_download http://zlib.net/zlib-1.2.13.tar.gz zlib.tar.gz
 do_download http://zlib.net/zlib1213.zip zlib.zip
 
 
-do_download https://github.com/facebook/zstd/archive/refs/tags/v1.5.2.tar.gz zstd.tar.gz
-do_download https://github.com/facebook/zstd/archive/refs/tags/v1.5.2.zip zstd.zip
+do_download https://github.com/facebook/zstd/archive/refs/tags/v1.5.5.tar.gz zstd.tar.gz
+do_download https://github.com/facebook/zstd/archive/refs/tags/v1.5.5.zip zstd.zip
 
 do_download https://github.com/lzfse/lzfse/archive/refs/tags/lzfse-1.0.zip lzfse.zip
 do_download https://github.com/lzfse/lzfse/archive/refs/tags/lzfse-1.0.tar.gz lzfse.tar.gz
